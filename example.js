3c116d15a2828b08e671a79500b944cb58ef7489
Nathan Rajlich <nathan@tootallnate.net>
2019-01-24 19:03:03 -0800

    Get stream URL in example

--- example.js
+++ example.js
@@ -1,9 +1,9 @@
 const iheart = require('./');
 
 async function main() {
-  const { stations = [] } = await iheart.search('106 KMEL');
-  const stream = await iheart.streamURL(stations[0]);
-  console.log({stream});
+  const { stations: [ station ] } = await iheart.search('106 KMEL');
+  const stream = await iheart.streamURL(station);
+  console.log({ station, stream });
 }
 
 main().catch(err => {
