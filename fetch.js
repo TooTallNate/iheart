9617e9d788b2fd09a5365dce87b3de96654ddb37
Nathan Rajlich <nathan@tootallnate.net>
2019-01-24 17:49:01 -0800

    Prettier

--- fetch.js
+++ fetch.js
@@ -1,40 +1,40 @@
-const retry = require('async-retry')
-const _fetch = require('isomorphic-fetch')
-const createDebug = require('debug')
+const retry = require('async-retry');
+const _fetch = require('isomorphic-fetch');
+const createDebug = require('debug');
 
-const debug = createDebug('iheart:fetch')
+const debug = createDebug('iheart:fetch');
 
 const defaultRetryOpts = {
   retries: 10,
   minTimeout: 500,
   onRetry(err) {
-    debug('Retrying: %o', err)
+    debug('Retrying: %o', err);
   }
-}
+};
 
 /**
  * Wrapper around `fetch` with `async-retry`.
  */
 module.exports = function fetch(url, _opts) {
-  const opts = Object.assign({}, defaultRetryOpts, _opts)
+  const opts = Object.assign({}, defaultRetryOpts, _opts);
 
   return retry(async bail => {
-    const method = (opts.method || 'GET').toUpperCase()
-    debug(`${method} ${url}`)
+    const method = (opts.method || 'GET').toUpperCase();
+    debug(`${method} ${url}`);
 
-    const res = await _fetch(url, opts)
+    const res = await _fetch(url, opts);
     if (!res.ok) {
       const err = new Error(
         `${method} ${url} failed: HTTP status code ${res.status}`
-      )
+      );
       if (Math.floor(res.status / 100) === 5) {
         // retry
-        throw err
+        throw err;
       } else {
         // assume 4xx, user error, don't retry
-        return bail(err)
+        return bail(err);
       }
     }
-    return res
-  }, opts)
-}
+    return res;
+  }, opts);
+};
--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const { extname } = require('path')
-const createDebug = require('debug')
-const { parse, format } = require('url')
-const { parse: parseIni } = require('ini')
+const { extname } = require('path');
+const createDebug = require('debug');
+const { parse, format } = require('url');
+const { parse: parseIni } = require('ini');
 
-const fetch = require('./fetch')
+const fetch = require('./fetch');
 
-const debug = createDebug('iheart')
+const debug = createDebug('iheart');
 
 // go through a CORS reverse proxy so that these APIs work in the web browser
-const corsDefaultProxy = process.browser ? 'https://cors.now.sh/' : ''
+const corsDefaultProxy = process.browser ? 'https://cors.now.sh/' : '';
 
-const searchBase  = 'https://api.iheart.com/api/v1/catalog/searchAll'
-const streamsBase = 'https://api.iheart.com/api/v2/content/liveStations/'
+const searchBase = 'https://api.iheart.com/api/v1/catalog/searchAll';
+const streamsBase = 'https://api.iheart.com/api/v2/content/liveStations/';
 
 function getProxyURL(options) {
   return options && 'string' === typeof options.proxy
     ? options.proxy
-    : options && null === options.proxy ? '' : corsDefaultProxy
+    : options && null === options.proxy
+    ? ''
+    : corsDefaultProxy;
 }
 
 /**
@@ -24,37 +26,37 @@ function getProxyURL(options) {
  */
 async function search(keyword, options) {
   if ('string' !== typeof keyword) {
-    throw new TypeError('a string "keyword" is required')
+    throw new TypeError('a string "keyword" is required');
   }
 
-  const corsProxy = getProxyURL(options)
-  const formatted = parse(searchBase, true)
-  formatted.query.keywords = keyword
-  const url = format(formatted)
+  const corsProxy = getProxyURL(options);
+  const formatted = parse(searchBase, true);
+  formatted.query.keywords = keyword;
+  const url = format(formatted);
 
-  const res = await fetch(`${corsProxy}${url}`)
+  const res = await fetch(`${corsProxy}${url}`);
 
-  const body = await res.json()
+  const body = await res.json();
   if (body.errors) {
     // body.firstError
   }
-  return body
+  return body;
 }
 
 /**
  * Searches for a station by Id
  */
 async function getById(_station, options) {
-  const id = _station.id || _station
+  const id = _station.id || _station;
   if ('number' !== typeof id) {
-    throw new TypeError('a number station "id" is required')
+    throw new TypeError('a number station "id" is required');
   }
 
-  const corsProxy = getProxyURL(options)
-  let url = streamsBase + id
-  const res = await fetch(`${corsProxy}${url}`)
+  const corsProxy = getProxyURL(options);
+  let url = streamsBase + id;
+  const res = await fetch(`${corsProxy}${url}`);
 
-  const body = await res.json()
+  const body = await res.json();
   if (body.errors) {
     // body.firstError
   }
@@ -66,9 +68,9 @@ async function getById(_station, options) {
  * Gets the raw stream URL of the given Station or Station ID.
  */
 async function streamURL(_station, options) {
-  const { streams } = await getById(_station, options)
+  const { streams } = await getById(_station, options);
   if (!streams) {
-    throw new Error('No `streams` given')
+    throw new Error('No `streams` given');
   }
 
   url =
@@ -77,18 +79,18 @@ async function streamURL(_station, options) {
     streams.stw_stream ||
     streams.secure_shoutcast_stream ||
     streams.shoutcast_stream ||
-    streams[Object.keys(streams)[0]]
+    streams[Object.keys(streams)[0]];
 
   if ('.pls' === extname(url).toLowerCase()) {
-    debug('attempting to resolve "pls" file %o', url)
-    const res = await fetch(`${corsProxy}${url}`)
-    const body = parseIni(await res.text())
-    debug('pls file: %O', body)
-    url = body.playlist.File1
+    debug('attempting to resolve "pls" file %o', url);
+    const res = await fetch(`${corsProxy}${url}`);
+    const body = parseIni(await res.text());
+    debug('pls file: %O', body);
+    url = body.playlist.File1;
   }
 
-  debug('stream URL: %o', url)
-  return url
+  debug('stream URL: %o', url);
+  return url;
 }
 
 module.exports = {
